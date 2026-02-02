c29deae2ef4f0aaeaacc7a4fd0c0a45d33706922
Gabriel Alfonso <gabalfa@gmail.com>
2026-02-01 19:40:26 -0500

    testing theopengraph

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,10 @@
 
 export const SITE_TITLE = 'Alfa Botánica';
 export const SITE_DESCRIPTION = 'Bievenidos a Alfa Botánica, donde la pureza de la naturaleza se encuentra con el cuidado personal consciente.';
-export const WHATSAPP_NUMBER = '573003161189'; // Replace with actual WhatsApp number
+export const SITE_WHATSAPP = '573003161189';
+export const SITE_URL = 'https://alfabotanica.com'; // Replace with actual production URL
+export const SITE_OWNER = 'Alfabotánica';
+export const SITE_EMAIL = 'contacto@alfabotanica.com';
+export const SITE_LOGO = '/favicon.png'; // Path to site logo
+export const SITE_TWITTER_CREATOR = '@alfabotanica';
+export const SITE_TWITTER_SITE = '@alfabotanica';
