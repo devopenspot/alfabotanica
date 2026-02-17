424b69babca09a6c73585cbd1d1a578a6039aad9
Gabriel Alfonso <gabalfa@gmail.com>
2026-02-17 18:12:24 -0500

    added Astro framework skill and rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://alfabotanica.com',
+	site: 'https://alfabotanica.vercel.app/',
 	integrations: [mdx(), sitemap(), tailwind()],
 });
